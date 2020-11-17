d5bf1eb025a5f3cc5819104cbc823f2e76f886c7
Yang Li <git@netcrop.org>
2020-11-17 00:51:08 +0000

    2020-11-17 08:51:07

---
+++ src/stimy.py
@@ -0,0 +1,84 @@
+#!/bin/env -S PATH=/usr/local/bin:/usr/bin python3 -I
+import re,tempfile,resource,glob,io,subprocess,sys
+import os,socket,getpass,random,datetime
+class Stimy:
+    def __init__(self,*argv):
+        self.message = {'-h':' print this help message.',
+        '-t': 'test'}
+        self.argv = argv
+        self.args = argv[0]
+        self.argc = len(self.args)
+        if self.argc == 1: self.usage()
+        self.option = { '-h':self.usage ,'-t':self.test}
+        self.uid = os.getuid()
+        self.username = getpass.getuser()
+        self.homedir = os.environ.get('HOME') + '/'
+        self.tmpdir = '/var/tmp/'
+        self.debugging = DEBUGGING
+
+    def test(self):
+        with tempfile.NamedTemporaryFile(mode='w+',
+        dir=self.tmpdir,delete=False) as self.testfh:
+            self.testfh.write('big')
+        proc = self.run(cmd='cat',stdout=subprocess.PIPE,infile=self.testfh.name)
+        if proc != None:print(proc.stdout)
+        proc = self.run(stdout=subprocess.PIPE,infile='/etc/hostname')
+        if proc != None:print(proc.stdout)
+        self.run(cmd='date -u')
+
+    def run(self, cmd='',infile='',outfile='',stdin=None,stdout=None,
+        text=True,pass_fds=[],exit_errorcode='',shell=False):
+        try:
+            proc = None
+            emit = __file__ + ':' + sys._getframe(1).f_code.co_name + ':' \
+            + str(sys._getframe(1).f_lineno)
+            if infile != '': stdin = open(infile,'r')
+            if outfile != '': stdout = open(outfile,'w')
+            proc = subprocess.run(cmd.split(),
+            stdin=stdin,stdout=stdout,text=text,check=True,
+            pass_fds=pass_fds,shell=shell)
+            if infile != '': stdin.close() 
+            if outfile != '': stdout.close()
+            if not isinstance(proc,subprocess.CompletedProcess):
+                self.debug(info='end 1',emit=emit)
+                return None
+            if isinstance(proc.stdout,str):
+                proc.stdout = proc.stdout.rstrip('\n')
+                self.debug(info='end 2',emit=emit)
+                return proc
+        except subprocess.CalledProcessError as e:
+            emit += ':' + str(e.returncode)
+            if exit_errorcode == '':
+                if e.returncode != 0:
+                    self.debug(info='end 3: ',emit=emit)
+                    exit(1)
+            elif e.returncode == exit_errorcode:
+                self.debug(info='end 4',emit=emit)
+                exit(1)
+            return None
+
+    def debug(self,info='',outfile='',emit=''):
+        if not self.debugging: return
+        emit = sys._getframe(1).f_code.co_name + ':' \
+        + str(sys._getframe(1).f_lineno) + ':' + info + ':' + emit
+        print(emit)
+
+if __name__ == '__main__':
+    stimy = Stimy(sys.argv)
+    if stimy.args[1] not in stimy.option: stimy.usage()
+    try:
+        stimy.option[stimy.args[1]]()
+    except KeyboardInterrupt:
+        stimy.debug(info='user ctrl-C')
+    finally:
+        stimy.debug(info='session finally end')
+        for key,value in stimy.__dict__.items():
+            if isinstance(value,io.TextIOWrapper):
+                value.close()
+                continue
+            if isinstance(value,tempfile._TemporaryFileWrapper):
+                value.close() 
+                if os.access(value.name,os.R_OK): os.unlink(value.name)
+        if stimy.debugging:
+            with open('/tmp/stimylog','w') as stimy.logfh:
+                print(stimy.__dict__,file=stimy.logfh)
